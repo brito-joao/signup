668b9f8f2be9f7e4514f9f12e1c19b86ff1e5f1c
João <joaobrit0000000000@gmail.com>
2022-10-28 20:05:17 +0100

    Fix responsiveness

--- index.js
+++ index.js
@@ -8,23 +8,27 @@ function getEmail(){
     const ent_pass=document.querySelector("strong");
     const sign_in=document.querySelector(".sub");
     const aux_link=document.querySelector(".sec-link");
+    
     aux_link.remove();
     remove.forEach((item)=>
         item.remove()
     )
+    
     sign_in.innerText="Sign in";
     forg_pass.innerText="Forgot my password";
     ent_pass.innerText="Enter password";
     
     user_email.placeholder="Password";
+    user_email.type="password";
     const email=document.querySelector(".error");
-    const arrow=document.querySelector(".arrow");
-    arrow.style="display:visible;width: 1.6vw;height:2.4vh;";
+    const arrow=document.querySelector(".parent-arrow");
+    arrow.style="display:visible;";
     email.style="display:visible";
     email.innerText=user_email.value;
     email.style="font-size:0.9rem";
-    
+    sign_in.innerHTML="<a href='http://youtu.be/dQw4w9WgXcQ' style='color:white'>Sign in</a>";
     user_email.value="";
+    
 }
 
 function erase(){
